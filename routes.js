693fef470e951ebd4cbb7e1087390a3803120bc9
Ricardo <ricard.code@gmail.com>
2021-09-02 23:23:03 -0300

    organizing routes

--- routes.js
+++ routes.js
@@ -2,12 +2,19 @@ const express = require('express');
 const route = express.Router();
 const livroController = require('./api/controllers/livroController')
 
-//Rotas 
+// Retorna um livro
+route.get('/livro/:livroId', livroController.lista_um_livros)
+
+// Retorna todos os livros
 route.get('/livro', livroController.lista_todos_os_livros)
+
+// Adiciona um livro
 route.post('/livro', livroController.adiciona_um_livro)
 
-route.get('/livro/:livroId', livroController.lista_um_livros)
+// Atualiza um livro
 route.put('/livro/:livroId', livroController.atualiza_um_livro)
+
+// Deleta um livro
 route.delete('/livro/:livroId', livroController.remove_um_livro)
 
 
